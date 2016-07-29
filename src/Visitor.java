c915d91aced5de727ea360cead8aec8d2c164202
MShultz <mshultz@student.neumont.edu>
2016-07-29 14:29:54 -0600

    Completed visitor class. Initial completion.

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,18 +1,41 @@
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Visitor implements Traversal {
+	String hashedIndexFile = "Indexed.bin";
 	URL currentUrl;
-	String currentLine;
+	String pattern = "(<.*>)?(?<Content>([^\\/<])+)(<\\/.*>)?";
+	Pattern wordPattern;
+	WordIndex index;
+
+	public Visitor() {
+		wordPattern = Pattern.compile(pattern);
+		index = new WordIndex(hashedIndexFile);
+	}
 
 	@Override
 	public void getURL(URL url) {
 		currentUrl = url;
-		
+
 	}
 
 	@Override
 	public void getLine(String line) {
-		currentLine = line;
+		addWordsToIndex(getWords(line));
 	}
 
+	private String[] getWords(String line) {
+		Matcher match = wordPattern.matcher(line);
+		match.find();
+		String text = match.group("Content");
+		text = text.replaceAll("[,!.]", " ").trim();
+		return text.split("\\s+");
+
+	}
+	private void addWordsToIndex(String[] words){
+		for(String s: words){
+			index.add(s, currentUrl.toString());
+		}
+	}
 }
